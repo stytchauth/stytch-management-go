bb01142e384210c887e90a9cd4b3662e1c40f4b5
Bruno Peynetti <bpeynetti@stytch.com>
2024-07-17 16:46:07 -0400

    wip

--- pkg/api/emailtemplates.go
+++ pkg/api/emailtemplates.go
@@ -1,1 +1,72 @@
 package api
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/stytchauth/stytch-management-go/v1/pkg/api/internal"
+	"github.com/stytchauth/stytch-management-go/v1/pkg/models/emailtemplates"
+)
+
+type EmailTemplatesClient struct {
+	client *internal.Client
+}
+
+func newEmailTemplatesClient(c *internal.Client) *EmailTemplatesClient {
+	return &EmailTemplatesClient{client: c}
+}
+
+func (c *EmailTemplatesClient) GetAll(ctx context.Context, body emailtemplates.GetAllEmailTemplatesRequest) (*emailtemplates.GetAllEmailTemplatesResponse, error) {
+	var resp emailtemplates.GetAllEmailTemplatesResponse
+	err := c.client.NewRequest(
+		ctx,
+		"GET",
+		fmt.Sprintf("/v1/projects/%s/email_templates", body.LiveProjectID),
+		nil,
+		nil,
+		&resp)
+
+	return &resp, err
+}
+
+func (c *EmailTemplatesClient) Get(ctx context.Context, body emailtemplates.GetEmailTemplateRequest) (*emailtemplates.GetEmailTemplateResponse, error) {
+	var resp emailtemplates.GetEmailTemplateResponse
+	err := c.client.NewRequest(
+		ctx,
+		"GET",
+		fmt.Sprintf("/v1/projects/%s/email_templates/%s", body.ProjectID, body.EmailTemplateID),
+		nil,
+		nil,
+		&resp)
+
+	return &resp, err
+}
+
+func (c *EmailTemplatesClient) Create(ctx context.Context, body emailtemplates.CreateEmailTemplateRequest) (*emailtemplates.CreateEmailTemplateResponse, error) {
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	var res emailtemplates.CreateEmailTemplateResponse
+	err = c.client.NewRequest(
+		ctx,
+		"POST",
+		fmt.Sprintf("/v1/projects/%s/email_templates", body.LiveProjectID),
+		nil,
+		jsonBody,
+		&res)
+	return &res, err
+}
+
+func (c *EmailTemplatesClient) Delete(ctx context.Context, body emailtemplates.DeleteEmailTemplateRequest) (*emailtemplates.DeleteEmailTemplateResponse, error) {
+	var res emailtemplates.DeleteEmailTemplateResponse
+	err := c.client.NewRequest(
+		ctx,
+		"DELETE",
+		fmt.Sprintf("/v1/projects/%s/email_templates/%s", body.LiveProjectID, body.EmailTemplateID),
+		nil,
+		nil,
+		&res)
+	return &res, err
+}
--- pkg/models/emailtemplates/types.go
+++ pkg/models/emailtemplates/types.go
@@ -1,31 +1,64 @@
 package emailtemplates
 
 type EmailTemplate struct {
+	ID                 string `json:"id"`
+	ProjectID          string `json:"project_id"`
+	Type               string `json:"type"`
+	IsDefault          bool   `json:"is_default"`
+	Method             string `json:"method"`
+	Name               string `json:"name"`
+	VanityID           string `json:"vanity_id"`
+	Subject            string `json:"subject"`
+	PlaintextContent   string `json:"plaintext_content"`
+	HTMLContent        string `json:"html_content"`
+	ButtonColor        string `json:"button_color"`
+	ButtonTextColor    string `json:"button_text_color"`
+	FontFamily         string `json:"font_family"`
+	TextAlignment      string `json:"text_alignment"`
+	LogoSrc            string `json:"logo_src"`
+	ButtonBorderRadius string `json:"button_border_radius"`
+	FromLocalPart      string `json:"from_local_part"`
+	FromDomain         string `json:"from_domain"`
+	FromName           string `json:"from_name"`
+	ReplyToLocalPart   string `json:"reply_to_local_part"`
+	ReplyToName        string `json:"reply_to_name"`
+	SecondarySubject   string `json:"secondary_subject"`
+}
+
+type EmailTemplateDefault struct {
+	Type                      string `json:"type"`
+	Locale                    string `json:"locale"`
+	HTMLContent               string `json:"html_content"`
+	PlaintextContent          string `json:"plaintext_content"`
+	Subject                   string `json:"subject"`
+	SecondarySubject          string `json:"secondary_subject"`
+	DiscoveryHTMLContent      string `json:"discovery_html_content"`
+	DiscoveryPlaintextContent string `json:"discovery_plaintext_content"`
 }
 
 type TestAndLiveEmailTemplate struct {
-	LiveEmailTemplate EmailTemplate `json:"live_email_template"`
 	TestEmailTemplate EmailTemplate `json:"test_email_template"`
+	LiveEmailTemplate EmailTemplate `json:"live_email_template"`
 }
 
 type GetAllEmailTemplatesRequest struct {
-	LiveProjectID string `json:"live_project_id"`
+	LiveProjectID string `json:"project_id"`
 }
 
-type GetEmailTemplatesResponse struct {
+type GetAllEmailTemplatesResponse struct {
 	StatusCode       int                        `json:"status_code"`
 	RequestID        string                     `json:"request_id"`
 	LiveProjectID    string                     `json:"project_id"`
 	EmailTemplates   []TestAndLiveEmailTemplate `json:"email_templates"`
-	DefaultTemplates []EmailTemplate            `json:"defaults"`
+	DefaultTemplates []EmailTemplateDefault     `json:"defaults"`
 }
 
 type CreateEmailTemplateRequest struct {
-	ProjectID string `json:"project_id"`
-	Name      string `json:"name"`
-	VanityID  string `json:"vanity_id"`
-	Method    string `json:"method"`
-	Type      string `json:"type"`
+	LiveProjectID string `json:"project_id"`
+	Name          string `json:"name"`
+	VanityID      string `json:"vanity_id"`
+	Method        string `json:"method"`
+	Type          string `json:"type"`
 }
 
 type CreateEmailTemplateResponse struct {
@@ -62,7 +95,13 @@ type GetEmailTemplateResponse struct {
 }
 
 type DeleteEmailTemplateRequest struct {
-	// XCXC Not sure if live or any
 	LiveProjectID   string `json:"project_id"`
 	EmailTemplateID string `json:"email_template_id"`
+}
+
+type DeleteEmailTemplateResponse struct {
+	StatusCode          int    `json:"status_code"`
+	RequestID           string `json:"request_id"`
+	EmailTemplateID     string `json:"email_template_id"`
+	TestEmailTemplateID string `json:"test_email_template_id"`
 }
