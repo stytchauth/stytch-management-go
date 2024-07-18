7efe8b06602e93ee7611af8782aa4194450c8915
Bruno Peynetti <bpeynetti@stytch.com>
2024-07-18 11:18:16 -0400

    Stubs for tests

--- pkg/api/emailtemplates_test.go
+++ pkg/api/emailtemplates_test.go
@@ -1,1 +1,32 @@
-package api
+package api_test
+
+import (
+	"testing"
+)
+
+func TestEmailTemplatesClient_Create(t *testing.T) {
+	// Arrange
+	// Assert
+	t.Skip("Not implemented.")
+}
+
+func TestEmailTemplatesClient_Get(t *testing.T) {
+	// Arrange
+	// Act
+	// Assert
+	t.Skip("Not implemented.")
+}
+
+func TestEmailTemplatesClient_GetAll(t *testing.T) {
+	// Arrange
+	// Act
+	// Assert
+	t.Skip("Not implemented")
+}
+
+func TestEmailTemplatesClient_Update(t *testing.T) {
+	// Arrange
+	// Act
+	// Assert
+	t.Skip("Not implemented")
+}
