dfda7766d4d5fd97b64cb41592df098bd07a928d
Bruno Peynetti <bpeynetti@stytch.com>
2024-07-18 11:14:55 -0400

    Improve types

--- pkg/models/emailtemplates/types.go
+++ pkg/models/emailtemplates/types.go
@@ -1,39 +1,54 @@
 package emailtemplates
 
+type Method string
+type TemplateType string
+
+const (
+	MethodCustomHTML                  Method       = "custom_html"
+	MethodBuiltInCustomizations       Method       = "built_in_customizations"
+	TemplateTypeLogin                 TemplateType = "login"
+	TemplateTypeSignup                TemplateType = "signup"
+	TemplateTypeInvite                TemplateType = "invite"
+	TemplateTypeResetPassword         TemplateType = "reset_password"
+	TemplateTypeOneTimePasscode       TemplateType = "one_time_passcode"
+	TemplateTypeOneTimePasscodeSignup TemplateType = "one_time_passcode_signup"
+	TemplateTypeAll                   TemplateType = "all"
+)
+
 type EmailTemplate struct {
-	ID                 string `json:"id"`
-	ProjectID          string `json:"project_id"`
-	Type               string `json:"type"`
-	IsDefault          bool   `json:"is_default"`
-	Method             string `json:"method"`
-	Name               string `json:"name"`
-	VanityID           string `json:"vanity_id"`
-	Subject            string `json:"subject"`
-	PlaintextContent   string `json:"plaintext_content"`
-	HTMLContent        string `json:"html_content"`
-	ButtonColor        string `json:"button_color"`
-	ButtonTextColor    string `json:"button_text_color"`
-	FontFamily         string `json:"font_family"`
-	TextAlignment      string `json:"text_alignment"`
-	LogoSrc            string `json:"logo_src"`
-	ButtonBorderRadius string `json:"button_border_radius"`
-	FromLocalPart      string `json:"from_local_part"`
-	FromDomain         string `json:"from_domain"`
-	FromName           string `json:"from_name"`
-	ReplyToLocalPart   string `json:"reply_to_local_part"`
-	ReplyToName        string `json:"reply_to_name"`
-	SecondarySubject   string `json:"secondary_subject"`
+	ID                 string       `json:"id"`
+	ProjectID          string       `json:"project_id"`
+	Type               TemplateType `json:"type"`
+	IsDefault          bool         `json:"is_default"`
+	Method             Method       `json:"method"`
+	Name               string       `json:"name"`
+	VanityID           string       `json:"vanity_id"`
+	Subject            string       `json:"subject"`
+	PlaintextContent   string       `json:"plaintext_content"`
+	HTMLContent        string       `json:"html_content"`
+	ButtonColor        string       `json:"button_color"`
+	ButtonTextColor    string       `json:"button_text_color"`
+	FontFamily         string       `json:"font_family"`
+	TextAlignment      string       `json:"text_alignment"`
+	LogoSrc            string       `json:"logo_src"`
+	ButtonBorderRadius string       `json:"button_border_radius"`
+	FromLocalPart      string       `json:"from_local_part"`
+	FromDomain         string       `json:"from_domain"`
+	FromName           string       `json:"from_name"`
+	ReplyToLocalPart   string       `json:"reply_to_local_part"`
+	ReplyToName        string       `json:"reply_to_name"`
+	SecondarySubject   string       `json:"secondary_subject"`
 }
 
 type EmailTemplateDefault struct {
-	Type                      string `json:"type"`
-	Locale                    string `json:"locale"`
-	HTMLContent               string `json:"html_content"`
-	PlaintextContent          string `json:"plaintext_content"`
-	Subject                   string `json:"subject"`
-	SecondarySubject          string `json:"secondary_subject"`
-	DiscoveryHTMLContent      string `json:"discovery_html_content"`
-	DiscoveryPlaintextContent string `json:"discovery_plaintext_content"`
+	Type                      TemplateType `json:"type"`
+	Locale                    string       `json:"locale"`
+	HTMLContent               string       `json:"html_content"`
+	PlaintextContent          string       `json:"plaintext_content"`
+	Subject                   string       `json:"subject"`
+	SecondarySubject          string       `json:"secondary_subject"`
+	DiscoveryHTMLContent      string       `json:"discovery_html_content"`
+	DiscoveryPlaintextContent string       `json:"discovery_plaintext_content"`
 }
 
 type TestAndLiveEmailTemplate struct {
@@ -54,11 +69,11 @@ type GetAllEmailTemplatesResponse struct {
 }
 
 type CreateEmailTemplateRequest struct {
-	LiveProjectID string `json:"project_id"`
-	Name          string `json:"name"`
-	VanityID      string `json:"vanity_id"`
-	Method        string `json:"method"`
-	Type          string `json:"type"`
+	LiveProjectID string       `json:"project_id"`
+	Name          string       `json:"name"`
+	VanityID      string       `json:"vanity_id"`
+	Method        Method       `json:"method"`
+	Type          TemplateType `json:"type"`
 }
 
 type CreateEmailTemplateResponse struct {
