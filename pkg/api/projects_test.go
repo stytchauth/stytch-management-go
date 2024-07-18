611af5e2e5848786254b0b33a545150809edb54b
Logan Gore <119902778+logan-stytch@users.noreply.github.com>
2024-07-18 08:52:58 -0700

    [BACK-3702, BACK-3703] Project Password endpoints (#8)

--- pkg/api/projects_test.go
+++ pkg/api/projects_test.go
@@ -2,6 +2,7 @@ package api_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,4 +46,67 @@ func Test_ProjectsGet(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "Get project test", resp.Project.ProjectSettings.ProjectName)
 	assert.Equal(t, projects.VerticalB2B, resp.Project.Vertical)
+}
+
+func Test_ProjectsGetPasswordStrengthPolicy(t *testing.T) {
+	// Arrange
+	client := NewTestClient(t)
+	ctx := context.Background()
+	createResp, err := client.Projects.Create(ctx, projects.CreateRequest{
+		ProjectName: "Get PW policy project test",
+		Vertical:    projects.VerticalB2B,
+	})
+	require.NoError(t, err)
+	_, err = client.Projects.SetPasswordStrengthPolicy(ctx, projects.SetPasswordStrengthPolicyRequest{
+		ProjectID: createResp.Project.ProjectID,
+		PasswordConfig: projects.PasswordStrengthConfig{
+			CheckBreachOnCreate:         true,
+			CheckBreachOnAuthentication: true,
+			ValidateOnAuthentication:    true,
+			ValidationPolicy:            projects.PasswordValidationPolicyLuds,
+			LudsMinCount:                ptr(12),
+			LudsComplexity:              ptr(3),
+		},
+	})
+	require.NoError(t, err)
+
+	// Act
+	resp, err := client.Projects.GetPasswordStrengthPolicy(ctx, projects.GetPasswordStrengthPolicyRequest{
+		ProjectID: createResp.Project.ProjectID,
+	})
+
+	// Assert
+	assert.NoError(t, err)
+	assert.True(t, resp.PasswordConfig.CheckBreachOnCreate)
+	fmt.Printf("Resp is %+v\n\n\n", resp)
+	assert.Equal(t, 12, *resp.PasswordConfig.LudsMinCount)
+}
+
+func Test_ProjectsSetPasswordStrengthPolicy(t *testing.T) {
+	// Arrange
+	client := NewTestClient(t)
+	ctx := context.Background()
+	createResp, err := client.Projects.Create(ctx, projects.CreateRequest{
+		ProjectName: "Set PW policy project test",
+		Vertical:    projects.VerticalB2B,
+	})
+	require.NoError(t, err)
+
+	// Act
+	resp, err := client.Projects.SetPasswordStrengthPolicy(ctx, projects.SetPasswordStrengthPolicyRequest{
+		ProjectID: createResp.Project.ProjectID,
+		PasswordConfig: projects.PasswordStrengthConfig{
+			CheckBreachOnCreate:         true,
+			CheckBreachOnAuthentication: true,
+			ValidateOnAuthentication:    true,
+			ValidationPolicy:            projects.PasswordValidationPolicyLuds,
+			LudsMinCount:                ptr(12),
+			LudsComplexity:              ptr(3),
+		},
+	})
+
+	// Assert
+	assert.NoError(t, err)
+	assert.True(t, resp.PasswordConfig.CheckBreachOnCreate)
+	assert.Equal(t, 12, *resp.PasswordConfig.LudsMinCount)
 }
