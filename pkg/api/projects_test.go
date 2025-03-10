d5525ee18b3bdb2d1ec52cc633892fe9433f173e
Brandon Gier <bgier@stytch.com>
2025-03-10 16:50:15 -0700

    Update names

--- pkg/api/projects_test.go
+++ pkg/api/projects_test.go
@@ -78,7 +78,7 @@ func Test_ProjectsUpdate(t *testing.T) {
 	// Arrange
 	client := NewTestClient(t)
 	project := client.DisposableProject(projects.VerticalB2B)
-	assert.False(t, project.LiveUseCrossOrgPasswords)
+	assert.False(t, project.LiveCrossOrgPasswordsEnabled)
 	ctx := context.Background()
 	newProjectName := "The new project v2"
 
@@ -94,5 +94,5 @@ func Test_ProjectsUpdate(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, newProjectName, resp.Project.Name)
 	assert.True(t, resp.Project.LiveUserImpersonationEnabled)
-	assert.True(t, resp.Project.LiveUseCrossOrgPasswords)
+	assert.True(t, resp.Project.LiveCrossOrgPasswordsEnabled)
 }
