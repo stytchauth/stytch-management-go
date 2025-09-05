c5bf6a175eaa9c1f2789c14ae2af9cd7b73e5c24
Bruno Peynetti <bpeynetti@stytch.com>
2025-09-05 15:23:20 -0400

    Add tests and fix type for password strength config

--- pkg/models/passwordstrengthconfig/types.go
+++ pkg/models/passwordstrengthconfig/types.go
@@ -28,11 +28,11 @@ type PasswordStrengthConfig struct {
 	// ValidationPolicy is the policy to use for password validation
 	ValidationPolicy ValidationPolicy `json:"validation_policy"`
 	// LudsMinPasswordLength is the minimum number of characters in a password if using a LUDS validation_policy. This field
-	// is ignored when using the ZXCVBN validation_policy. If present, this value must be a number in the range [8, 32].
-	LudsMinPasswordLength int `json:"luds_min_password_length"`
+	// is nil when using the ZXCVBN validation_policy.
+	LudsMinPasswordLength *int `json:"luds_min_password_length,omitempty"`
 	// LudsMinPasswordComplexity is the minimum number of "character types" in a password (Lowercase, Uppercase, Digits,
-	// Symbols) when using a LUDS validation_policy. If present, this must be a number in the range [1, 4].
-	LudsMinPasswordComplexity int `json:"luds_min_password_complexity"`
+	// Symbols) when using a LUDS validation_policy. This field is nil when using the ZXCVBN validation_policy.
+	LudsMinPasswordComplexity *int `json:"luds_min_password_complexity,omitempty"`
 }
 
 type GetRequest struct {
@@ -55,9 +55,25 @@ type SetRequest struct {
 	// Project is the project for which to set the password strength config
 	Project string `json:"-"`
 	// Environment is the environment (e.g., "test", "live") for which to set the password strength config
-	Environment string `json:"environment"`
-	// PasswordStrengthConfig is the new password strength configuration to be set for the project
-	PasswordStrengthConfig PasswordStrengthConfig `json:"password_strength_config"`
+	Environment string `json:"-"`
+	// CheckBreachOnCreation is a flag to check whether to use the HaveIBeenPwned database to detect password breaches
+	// when a user first creates their password.
+	CheckBreachOnCreation bool `json:"check_breach_on_creation"`
+	// CheckBreachOnAuthentication denotes whether to use the HaveIBeenPwned database to detect password breaches when
+	// a user authenticates.
+	CheckBreachOnAuthentication bool `json:"check_breach_on_authentication"`
+	// ValidateOnAuthentication notes whether to require a password reset on authentication if a user's current password
+	// no longer meets the project's current policy requirements.
+	ValidateOnAuthentication bool `json:"validate_on_authentication"`
+	// ValidationPolicy is the policy to use for password validation
+	ValidationPolicy ValidationPolicy `json:"validation_policy"`
+	// LudsMinPasswordLength is the minimum number of characters in a password if using a LUDS validation_policy. This field
+	// is ignored when using the ZXCVBN validation_policy. If present, this value must be a number in the range [8, 32].
+	LudsMinPasswordLength *int `json:"luds_min_password_length,omitempty"`
+	// LudsMinPasswordComplexity is the minimum number of "character types" in a password (Lowercase, Uppercase, Digits,
+	// Symbols) when using a LUDS validation_policy. This field is ignored when using the ZXCVBN validation_policy.
+	// If present, this must be a number in the range [1, 4].
+	LudsMinPasswordComplexity *int `json:"luds_min_password_complexity,omitempty"`
 }
 
 type SetResponse struct {
